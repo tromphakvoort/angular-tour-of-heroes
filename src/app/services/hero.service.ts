a70534f07b81f420270c647d17a4bca740e43d61
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-06-23 17:32:10 +0200

    Feature: Added Add hero crud

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -56,4 +56,11 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
